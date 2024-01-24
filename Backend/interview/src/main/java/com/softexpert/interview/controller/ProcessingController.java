28f6295156f1a06bb51ec3a1983ab7a9798537c2
Yonlero <abraaobrg150@gmail.com>
2024-01-24 13:27:39 -0300

    Feat: Validacao basica para interface de pagamento - Backend.

--- Backend/interview/src/main/java/com/softexpert/interview/controller/ProcessingController.java
+++ Backend/interview/src/main/java/com/softexpert/interview/controller/ProcessingController.java
@@ -2,13 +2,11 @@ package com.softexpert.interview.controller;
 
 import com.softexpert.interview.core.dtos.AmountDataInputDTO;
 import com.softexpert.interview.core.dtos.AmountDataOutputDTO;
+import com.softexpert.interview.core.enums.PaymentMethodsEnum;
 import com.softexpert.interview.service.ProcessAmountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +15,10 @@ public class ProcessingController {
     private final ProcessAmountService service;
 
     @PostMapping("/process")
-    public ResponseEntity<AmountDataOutputDTO> processAmount(@RequestBody AmountDataInputDTO amountDTO) {
-        AmountDataOutputDTO outputDTO = service.processAmountValues(amountDTO);
+    public ResponseEntity<AmountDataOutputDTO> processAmount(@RequestBody AmountDataInputDTO amountDTO,
+                                                             @RequestParam String paymentMethod) {
+        AmountDataOutputDTO outputDTO = service.processAmountValues(amountDTO,
+                PaymentMethodsEnum.valueOf(paymentMethod));
         return ResponseEntity.ok(outputDTO);
     }
 }
